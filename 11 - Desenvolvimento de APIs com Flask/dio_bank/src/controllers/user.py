523ace137be5eadeeacb33ecf86525c48ad9e114
Dawidson Pereira <dawidsongru@gmail.com>
2025-05-10 13:29:08 -0300

    Operações CRUD em APIs RESTful – UPDATE

--- 11 - Desenvolvimento de APIs com Flask/dio_bank/src/controllers/user.py
+++ 11 - Desenvolvimento de APIs com Flask/dio_bank/src/controllers/user.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from flask import Blueprint, request
+from sqlalchemy import inspect
 from app import User, db
 
 # localhost:5000/users
@@ -39,6 +40,34 @@ def handle_user():
 @app.route("/<int:user_id>")
 def get_user(user_id):
     user = db.get_or_404(User, user_id)
+    return {
+        "id": user.id,
+        "username": user.username,
+    }
+
+# Diferença entre PATCH e PUT
+# PATCH: Atualiza parcial (Atualiza só o title, só o body, só o datetime, só o author_id ou tudo, se quiser)
+# PUT: Atualiza tudo (title, body, datetime e author_id)
+# O PATCH é mais utilizado que o PUT
+
+@app.route("/<int:user_id>", methods=["PATCH"])
+def update_user(user_id):
+    user = db.get_or_404(User, user_id)
+    data = request.json
+
+    # uma forma de fazer (versão mais simples)
+    # if "username" in data:
+    #     user.username = data["username"]
+    #     db.session.commit()
+
+    # outra forma de fazer (versão dinâmica melhorada)
+    mapper = inspect(User)
+    for column in mapper.attrs:
+        if column.key in data:
+            setattr(user, column.key, data[column.key])
+    db.session.commit()
+
+
     return {
         "id": user.id,
         "username": user.username,
