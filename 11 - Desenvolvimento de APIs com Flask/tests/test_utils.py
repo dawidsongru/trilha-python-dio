62f43434f6e517ff5580c33ae8105dc243427007
Dawidson Pereira <dawidsongru@gmail.com>
2025-05-18 23:42:42 -0300

    Como mockar os dados com pytest-mock

--- 11 - Desenvolvimento de APIs com Flask/tests/test_utils.py
+++ 11 - Desenvolvimento de APIs com Flask/tests/test_utils.py
@@ -1,5 +1,4 @@
 from http import HTTPStatus
-from unittest.mock import Mock, patch
 
 import pytest
 from utils import eleva_quadrado, requires_role    # Importar os métodos
@@ -9,6 +8,7 @@ from utils import eleva_quadrado, requires_role    # Importar os métodos
 def test_eleva_quadrado_sucesso(test_input, expected):
     resultado = eleva_quadrado(test_input)      # Executar o método
     assert resultado == expected                # Esperar o retorno
+
 
 @pytest.mark.parametrize(
     "test_input,exc_class,msg",
@@ -22,41 +22,34 @@ def test_eleva_quadrado_falha(test_input, exc_class, msg):
         eleva_quadrado(test_input)
         # breakpoint()
     assert str(exc.value) == msg
-
 # Criando o método de teste
-def test_requires_role_success():   # cenário de sucesso
-    mock_user = Mock()
+def test_requires_role_success(mocker):   # cenário de sucesso
+    # Given (O que eu forneço para meu teste)
+    mock_user = mocker.Mock()
     mock_user.role.name = "admin"
 
-    mock_get_jwt_identity = patch("utils.get_jwt_identity")                     # não precisa fazer nenhum retorno
-    mock_db_get_or_404 = patch("utils.db.get_or_404", return_value=mock_user)   # precisa retornar um usuário
-    mock_get_jwt_identity.start()   # iniciar um recurso
-    mock_db_get_or_404.start()      # iniciar um recurso
+    mocker.patch("utils.get_jwt_identity")
+    mocker.patch("utils.db.get_or_404", return_value=mock_user)
+    decorated_function = requires_role("admin")(lambda: "success")
 
-    decorated_function = requires_role("admin")(lambda: "sucesso")
-
+    # When (O que eu executo)
     result = decorated_function()
 
-    assert result == "sucesso"
-
-    mock_get_jwt_identity.stop()    # parar um recurso
-    mock_db_get_or_404.stop()       # parar um recurso
+    # Then (O que eu verifico)
+    assert result == "success"
 
 
-def test_requires_role_fail():   # cenário de falha
-    mock_user = Mock()
+def test_requires_role_fail(mocker):   # cenário de falha
+    # Given (O que eu forneço para meu teste)
+    mock_user = mocker.Mock()
     mock_user.role.name = "normal"
 
-    mock_get_jwt_identity = patch("utils.get_jwt_identity")                     # não precisa fazer nenhum retorno
-    mock_db_get_or_404 = patch("utils.db.get_or_404", return_value=mock_user)   # precisa retornar um usuário
-    mock_get_jwt_identity.start()   # iniciar um recurso
-    mock_db_get_or_404.start()      # iniciar um recurso
-
-    decorated_function = requires_role("admin")(lambda: "sucesso")
-
+    # When (O que eu executo)
+    mocker.patch("utils.get_jwt_identity")
+    mocker.patch("utils.db.get_or_404", return_value=mock_user)
+    decorated_function = requires_role("admin")(lambda: "success")
+    
     result = decorated_function()
-
-    assert result == ({"message": "User haven't access."}, HTTPStatus.FORBIDDEN) # Tupla retorna dicionário e status
-
-    mock_get_jwt_identity.stop()    # parar um recurso
-    mock_db_get_or_404.stop()       # parar um recurso
+    
+    # Then (O que eu verifico)
+    assert result == ({"message": "User haven't access."}, HTTPStatus.FORBIDDEN)
