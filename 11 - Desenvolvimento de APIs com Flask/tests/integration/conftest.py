72e803bf105a7725ce6a13146e02b13e9144c943
Dawidson Pereira <dawidsongru@gmail.com>
2025-05-28 20:42:45 -0300

    Testes de integração - part3

--- 11 - Desenvolvimento de APIs com Flask/tests/integration/conftest.py
+++ 11 - Desenvolvimento de APIs com Flask/tests/integration/conftest.py
@@ -1,21 +1,34 @@
 import pytest
-from app import create_app, db
-
+from app import Role, User, create_app, db
 
 @pytest.fixture()   # Método que cria e devolve o app
 def app():
     app = create_app(
         {
             "SECRET_KEY": "test",
-            "SQLALCHEMY_DATABASE_URI": "sqlite://", # O banco de dados vai rodar em memória, ele não vai criar...
-            "JWT_SECRET_KEY": "test",               #... um arquivo físico, ele vai armazenar tudo na memória.
+            "SQLALCHEMY_DATABASE_URI": "sqlite://",     # O banco de dados vai rodar em memória, ele não vai criar...
+            "JWT_SECRET_KEY": "test",                   #... um arquivo físico, ele vai armazenar tudo na memória.
         }
     )
     with app.app_context():
-        db.create_all()
+        db.create_all()             
         yield app
+        # db.session.rollback()
+        db.drop_all()
 
-
-@pytest.fixture()
+@pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture()
+def access_token(client):
+    role = Role(name="admin")
+    db.session.add(role)
+    db.session.commit()
+
+    user = User(username="john-doe", password="test", role_id=role.id)
+    db.session.add(user)
+    db.session.commit()
+
+    response = client.post("/auth/login", json={"username": user.username, "password": user.password})
+    return response.json["access_token"]
--- 11 - Desenvolvimento de APIs com Flask/tests/integration/controllers/test_user.py
+++ 11 - Desenvolvimento de APIs com Flask/tests/integration/controllers/test_user.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from sqlalchemy import func
 from app import Role, User, db
 
 def test_get_user_success(client):              # Testando um comportamento de sucesso
@@ -32,3 +33,42 @@ def test_get_user_not_found(client):            # Testando um comportamento de f
 
     # Then (O que eu verifico)
     assert response.status_code == HTTPStatus.NOT_FOUND
+
+def test_create_user(client, access_token):                  # Teste criar um novo usuário
+    # Given (O que eu forneço para meu teste)
+    # # role_id = db.session.execute(db.select(Role.id).where(Role.name == "admin")).scalar()
+    # payload = {"username": "user2", "password": "user2", "role_id": 1}                # Criar os dados
+    role_id = db.session.execute(db.select(Role.id).where(Role.name == "admin")).scalar()
+    payload = {"username": "user2", "password": "user2", "role_id": role_id}                # Criar os dados
+
+    # When (O que eu executo)
+    response = client.post("/users/", json=payload, headers={"Authorization": f"Bearer {access_token}"})  # Endpoint que envia usuários (POST)
+
+    # Then (O que eu verifico)
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json == {"message": "User created!"}
+    assert db.session.execute(db.select(func.count(User.id))).scalar() == 2
+
+def test_list_users(client, access_token):                    # Teste list_user
+    # Given (O que eu forneço para meu teste)
+    user = db.session.execute(db.select(User).where(User.username == "john-doe")).scalar()
+    response = access_token = client.post("/auth/login", json={"username": user.username, "password": user.password})
+    access_token = response.json["access_token"]
+
+    # When (O que eu executo)
+    response = client.get("/users/", headers={"Authorization": f"Bearer {access_token}"})   # Endpoint que busca usuários (GET)
+    
+    # Then (O que eu verifico)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json == {
+        "users": [
+            {
+                "id": user.id,
+                "username": user.username,
+                "role": {
+                    "id": user.role_id,
+                    "name": user.role.name,
+                },
+            }
+        ]
+    }
